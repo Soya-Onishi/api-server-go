e4bb4d1223210e6286f5f9b0dfe4d1a62aab16c2
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-03-16 11:52:56 +0000

    Add db access profile

--- cmd/api-server-go/main.go
+++ cmd/api-server-go/main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/Soya-Onishi/api-server-go/internal/controller"
 	"github.com/Soya-Onishi/api-server-go/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
+type DBProfile struct {
+	user     string
+	password string
+	url      string
+	dbname   string
+}
+
+var profile = DBProfile{
+	user:     "app",
+	password: "app",
+	url:      "172.25.172.3:3306",
+	dbname:   "todo",
+}
+
 func setupServer() *controller.Router {
 	var db *sql.DB
 	var err error
 
-	db, err = sql.Open("mysql", "")
+	dsn := fmt.Sprintf(
+		"%v:%v@(%v)/%v",
+		profile.user,
+		profile.password,
+		profile.url,
+		profile.dbname,
+	)
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
