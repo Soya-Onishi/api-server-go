231bfeb906a26e2821674ed5efe6ff0985c26cb2
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-03-18 03:20:48 +0000

    Fix table name

--- build/db/sql/99_insert.sql
+++ build/db/sql/99_insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO todo (title) VALUES ('prepare hot water');
-INSERT INTO todo (title) VALUES ('wait for three minutes');
-INSERT INTO todo (title) VALUES ('eat ramen');
+INSERT INTO todo.todo_list (title) VALUES ('prepare hot water');
+INSERT INTO todo.todo_list (title) VALUES ('wait for three minutes');
+INSERT INTO todo.todo_list (title) VALUES ('eat ramen');
